bd882a30e95d980b80a557845a7a57d328f65c61
Fengi <fengbald@gmx.de>
2011-02-21 21:44:38 +0100

    added auto-complete

--- user/config.el
+++ user/config.el
@@ -69,6 +69,14 @@
 (set-cursor-color "light grey")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; load auto-complete
+(add-to-list 'load-path "~/.emacs.d/auto-complete")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/ac-dict")
+(ac-config-default)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; turn on icy-mode
 (setq load-path (cons (expand-file-name "~/.emacs.d/user/icicles") load-path))
 (require 'icicles)
