00ff500bf464693c83459f3b3f263509e8fe2aaa
Fengi <fengbald@gmx.de>
2011-03-04 18:26:54 +0100

    Reformatted user/config.el and set default dict.

--- user/config.el
+++ user/config.el
@@ -57,6 +57,9 @@
   (interactive)
   (my-change-dictionary "de"))
 
+;; set default dictionary to english
+(my-en-dictionary)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; custom key bindings
 
@@ -66,27 +69,27 @@
 (global-set-key [(ctrl c) (right)] 'windmove-right)
 (global-set-key [(ctrl c) (up)]    'windmove-up)
 (global-set-key [(ctrl c) (down)]  'windmove-down)
-(global-set-key [(f12)] 'other-window)
+(global-set-key [(f12)]            'other-window)
 
 ;; Buffer cycling key bindings
-(global-set-key [(ctrl f12)] 'ska-next-buffer)
-(global-set-key [(ctrl >)] 'ska-next-buffer)
+(global-set-key [(ctrl f12)]   'ska-next-buffer)
+(global-set-key [(ctrl >)]     'ska-next-buffer)
 (global-set-key [(ctrl S f12)] 'ska-previous-buffer)
-(global-set-key [(ctrl <)] 'ska-previous-buffer)
+(global-set-key [(ctrl <)]     'ska-previous-buffer)
 
-(global-set-key "\C-cm" 'menu-bar-mode)
-(global-set-key "\C-cl" 'goto-line)
-(global-set-key "\C-ct" 'toggle-truncate-lines)
-(global-set-key "\M-#"  'shell-command-on-region)
+(global-set-key [(ctrl c) (m)] 'menu-bar-mode)
+(global-set-key [(ctrl c) (l)] 'goto-line)
+(global-set-key [(ctrl c) (t)] 'toggle-truncate-lines)
+(global-set-key "\M-#"         'shell-command-on-region)
 (global-set-key [(ctrl c) (b)] 'comment-box)
 (global-set-key [(ctrl c) (c)] 'comment-or-uncomment-region)
-(global-set-key [(ctrl tab)] 'auto-complete)
+(global-set-key [(ctrl tab)]   'auto-complete)
 (global-set-key [(ctrl c) (g)] 'my-git-status)
-(global-set-key [(f5)] 'flyspell-buffer)
-(global-set-key [(f8)  (e)] 'my-en-dictionary )
-(global-set-key [(f8)  (d)] 'my-de-dictionary )
-(global-set-key "\C-c," 'rails/goto)
-(global-set-key [(ctrl |)] 'linum-mode)
+(global-set-key [(f5)]         'flyspell-buffer)
+(global-set-key [(f8)  (e)]    'my-en-dictionary )
+(global-set-key [(f8)  (d)]    'my-de-dictionary )
+(global-set-key [(ctrl c) (,)] 'rails/goto)
+(global-set-key [(ctrl |)]     'linum-mode)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; custom hooks
@@ -94,7 +97,7 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; custom look
-(column-number-mode)
+(column-number-mode t)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
